414f851d5c6213652496facabcaf1fbaab3e6f6e
Ihor Kochetov <i.kochetov@svitla.com>
2022-07-22 16:12:11 +0200

    Add data project and db context

---
+++ Books.Data/BooksContext.cs
@@ -0,0 +1,15 @@
+﻿using Books.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Books.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
