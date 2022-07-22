297a1f8490f7ea676d2c9498d270534c3723dfed
Ihor Kochetov <i.kochetov@svitla.com>
2022-07-22 14:19:30 +0200

    Add initial book definition and project structure

---
+++ Books.Domain/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Books.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int NumberOfCopies { get; set; }
+    }
+}
